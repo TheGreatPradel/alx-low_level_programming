ae6b102216a4ab35f44a73e8d6d6d07bbd6ef687
TheGreatPradel <pradelisgreat@gmail.com>
2022-06-07 20:27:28 +0100

    nested loop

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,11 +4,13 @@
  *
  */
 
-void print_alphabet (void)
+void print_alphabet(void)
 {
 	char letter;
-		for 
-			(letter = 'a'; letter <= 'z'; letter++)
-			_putchar (letter);
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+
+		_putchar(letter);
+	
 	_putchar ('\n');
 }
