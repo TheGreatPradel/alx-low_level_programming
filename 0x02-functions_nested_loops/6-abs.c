11f0d70c4fe579d8fc25fde904f7bf5d228df2d7
TheGreatPradel <pradelisgreat@gmail.com>
2022-06-07 23:32:25 +0100

    nested loop

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - compute the absolute value of an interger.
+ *
+ * @n: the interger to be compute
+ *
+ * Return: The absolute value of the interger
+ *
+ **/
+
+ int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
