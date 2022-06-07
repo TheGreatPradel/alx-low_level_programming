08a913bfc534efb03369af652b00d2a792cacc1a
TheGreatPradel <pradelisgreat@gmail.com>
2022-06-07 20:47:57 +0100

    nested loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10x the alphabet in lowercase.
+ */
+
+{
+void print_alphabet_x10(void)
+	int counter = 0;
+	
+	char letter;
+
+	while (counter++ <= 9)
+	{ 
+		for(letter = 'a'; letter <= 'z'; letter++ )
+			_putchar (letter)
+				
+				_putchar('\n')
+	}
+
+}
+
