f37507c6ab56d8740e726a13b9ceb9037c3b8976
TheGreatPradel <pradelisgreat@gmail.com>
2022-06-10 00:32:57 +0100

    more

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -13,8 +13,12 @@ void print_line(int n)
 	{
 		for (len = 0; len < n; len++)
 			_putchar('_');
+			_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
 	}
 
-	_putchar('\n');
-	return (0);
+		return 0
 }
